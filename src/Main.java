c42758ec3350239f04ce3b1770b8df33e51475f9
imre <imre@fien-dermout.nl>
2021-02-10 13:27:51 +0100

    pipo

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,13 @@
 public class Main {
-int pipo;
+    public static void main(String[] args) {
+        System.out.println("Test my methods:");
+        double a = 12.5;
+        double b = 88.8;
+        double r = sum(a, b);
+        System.out.println(a + " + " + b + " = " + r);
+    }
+    public static double sum (double a, double b) {
+        return a + b;
+    }
+
 }
