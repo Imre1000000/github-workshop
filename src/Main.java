399db760e879a1a94268b9905979bbd871c84ca7
imre <imre@fien-dermout.nl>
2021-02-10 13:47:05 +0100

    added method multiply

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,17 @@ public class Main {
         System.out.println("Test my methods:");
         double a = 12.5;
         double b = 88.8;
-        double r = sum(a, b);
-        System.out.println(a + " + " + b + " = " + r);
+        double r1 = sum(a, b);
+        System.out.println(a + " + " + b + " = " + r1);
+        double r2 = multiply(a, b);
+        System.out.println(a + " * " + b + " = " + r2);
     }
     public static double sum (double a, double b) {
         return a + b;
     }
+    public static double multiply (double a, double b) {
+        return a * b;
+    }
 
 }
+
